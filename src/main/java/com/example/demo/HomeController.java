4f79666cabf8ff791c3701f974124890ff46e5aa
bavatar <janderja@comcast.net>
2019-09-24 13:07:39 -0400

    Static Recipe Website

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/index")
+    public String homePage(){
+        return "index";
+    }
+
+    @RequestMapping("/list")
+    public String listPage(){
+        return "list";
+    }
+
+    @RequestMapping("/fruity-delicious")
+    public String listFruity(){
+        return "fruity-delicious";
+    }
+}
